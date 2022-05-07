f774f63135a956e19bcf22ff6ce9cf995ac2d551
Sara Gomes <sara.gomes@g.globo>
2022-05-07 11:23:10 -0300

    Add port env variable

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,6 @@ app.use(bodyParser.json({ limit: "1mb" }));
 app.use(express.json());
 app.use(routes);
 
-// porta 3333
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("HTTP server running!");
 });
